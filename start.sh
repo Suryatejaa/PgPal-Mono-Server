f3d0db52e8c9c6364cf2ac5c1e56f26c0379553e
Suryatejaa <illasuryanani2001@gmail.com>
2025-06-14 20:04:06 +0530

    CI/CD: setup GitHub Actions for auto deploy

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 # Run this script to update all services
-for service in auth-service property-service room-service complaint-service dashboard-service tenant-service kitchen-service gateway; do
+for service in auth-service property-service room-service complaint-service notification-service dashboard-service tenant-service kitchen-service gateway; do
   cd $service
   npm pkg set scripts.test="echo 'Tests not implemented yet' && exit 0"
   cd ..
